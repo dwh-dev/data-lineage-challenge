cfa5757e95d5fee3208834b8a7ee1db276ec2453
Anton Revyako <anton.revyako@gmail.com>
2023-12-11 17:32:25 +0100

    Snowflake Identifiers

---
+++ snowflake/1.identifiers/sql/1.identifiers.1.sql
@@ -0,0 +1,38 @@
+-- https://docs.snowflake.com/en/sql-reference/identifiers-syntax
+
+CREATE DATABASE DWHDEMO_DB;
+
+CREATE TABLE My$identifier (
+  f1 INT
+);
+
+CREATE TABLE _my_identifier (
+  f2 INT
+);
+
+CREATE TABLE "My 'Identifier'" (
+  f3 INT
+);
+
+CREATE TABLE "$Identifier" (
+  f4 INT
+);
+
+CREATE TABLE `Myidentifier` (
+  f5 INT
+);
+
+CREATE TABLE "quote""andunquote""" (
+  f6 INT
+);
+
+CREATE VIEW V1 AS 
+  SELECT * 
+  FROM 
+    My$identifier,
+    _my_identifier,
+    "My 'Identifier'", 
+    "$Identifier",
+    `Myidentifier`,
+    "quote""andunquote"""
+  ;
---
+++ snowflake/1.identifiers/sql/1.identifiers.2.sql
@@ -0,0 +1,76 @@
+SET db_var = 'DEMO_DB';
+SET schema_var = 'DEMO_DB.SCH1';
+SET table_var1 = 'T1';
+SET table_var2 = 'public.t3';
+SET tablefunction = 'tblfn';
+
+CREATE DATABASE identifier($db_var);
+CREATE SCHEMA identifier($schema_var);
+
+CREATE TABLE identifier($table_var1) (
+  i1 INT,
+  v1 VARIANT
+);
+CREATE TABLE identifier($table_var2) (
+  f3 INT
+);
+
+CREATE FUNCTION tblfn()
+RETURNS TABLE (
+  C1 INT, C2 VARIANT
+) AS
+  $$
+    SELECT
+      *
+    FROM
+      T1
+  $$
+;
+
+-- Object Identifier
+CREATE VIEW V1 AS
+  SELECT * 
+  FROM
+    identifier($table_var1)
+;
+
+CREATE VIEW V2 AS
+  SELECT *
+  FROM
+    TABLE(identifier($tablefunction)())
+;
+
+-- Column Identifier
+CREATE VIEW V3 AS
+  SELECT identifier('DEMO_DB.SCH1.T1.V1'):json_prop as prop 
+  FROM 
+    DEMO_DB.SCH1.T1
+;
+
+CREATE VIEW V4 AS
+  with t(x) as (
+    SELECT i1 FROM T1
+  )
+  SELECT identifier('t.x')
+  FROM t
+;
+
+-- Table Literal
+CREATE VIEW V5 AS
+  SELECT * 
+  FROM
+    table('t1')
+;
+
+CREATE VIEW V6 AS
+  SELECT *
+  FROM
+    table($$DEMO_DB."SCH1"."T1"$$)
+;
+
+-- Double-Dot Notation
+CREATE VIEW V7 AS
+  SELECT * 
+  FROM
+    DEMO_DB..T3
+;
