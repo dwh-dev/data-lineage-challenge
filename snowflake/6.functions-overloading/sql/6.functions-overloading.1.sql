73e6e7fb11335fa5ab5ec93851268981c8966a8a
Anton Revyako <anton.revyako@gmail.com>
2024-06-14 18:27:22 +0200

    fix names

--- snowflake/6.functions-overloading/sql/6.functions-overloading.1.sql
+++ snowflake/6.functions-overloading/sql/6.functions-overloading.1.sql
@@ -1,19 +1,19 @@
 CREATE TABLE A(ID INT);
 CREATE TABLE B(S STRING);
 
-CREATE OR REPLACE FUNCTION fn_override ( _id number )
+CREATE OR REPLACE FUNCTION fn_overload ( _id number )
   RETURNS TABLE (id int)
   AS 'select id from a where id > _id'
 ;
 
 
-CREATE OR REPLACE FUNCTION fn_override ( _s string )
+CREATE OR REPLACE FUNCTION fn_overload ( _s string )
   RETURNS TABLE (s string)
   AS 'select s from b where s != _s'
 ;
 
 CREATE VIEW V1 AS
-SELECT * FROM TABLE(fn_override(1));
+SELECT * FROM TABLE(fn_overload(1));
 
 CREATE VIEW V2 AS
-SELECT * FROM TABLE(fn_override('1'));
+SELECT * FROM TABLE(fn_overload('1'));
